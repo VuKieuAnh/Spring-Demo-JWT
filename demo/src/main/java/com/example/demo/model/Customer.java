08ab0fba3e14e4476c63b12ce86eb14659a6531a
VuKieuAnh <vukieuanh.hnue@gmail.com>
2024-01-10 16:23:31 +0700

    add model

--- demo/src/main/java/com/example/demo/model/Customer.java
+++ demo/src/main/java/com/example/demo/model/Customer.java
@@ -8,9 +8,12 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Role {
+public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private Integer age;
+    private String gender;
+    private String address;
 }
